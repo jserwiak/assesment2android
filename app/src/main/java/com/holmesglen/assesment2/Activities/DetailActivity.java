4661cb710e81086afac826ba3f897c8291d5b1c2
Jurek <jserwiak@gmail.com>
2021-04-30 16:18:43 +1000

    added button add

--- app/src/main/java/com/holmesglen/assesment2/Activities/DetailActivity.java
+++ app/src/main/java/com/holmesglen/assesment2/Activities/DetailActivity.java
@@ -1,6 +1,8 @@
 package com.holmesglen.assesment2.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -24,29 +26,41 @@ public class DetailActivity extends AppCompatActivity {
         TextView txtDoB = findViewById(R.id.txt_Detail_email);
 
         ArrayList<Contact> contactList;
+        contactList = PhonebookDb.getInstance().getAll();
+        Collections.sort(contactList);
         int index = -1;
         /*String first;
         String last;
         String phone;
         String dob;*/
         Bundle bundle = getIntent().getExtras();
-        if(bundle != null && bundle.containsKey("first"))
+        if(bundle != null && bundle.containsKey("contact_index"))
         {
             index = bundle.getInt("contact_index"); // this is getting the index of sorted array list from hashtable,
             //to get proper data we need to sort contacts.
-            contactList = PhonebookDb.getInstance().getAll();
-            Collections.sort(contactList);
-            Contact c = contactList.get(index);
+            if(index >= 0 && index <contactList.size()) {
 
-            txtFirst.setText(c.getFirstName());
-            txtLast.setText(c.getLastName());
-            txtPhone.setText(c.getPhone());
-            txtDoB.setText(c.getDateOfBirth());
+
+                Contact c = contactList.get(index);
+
+                txtFirst.setText(c.getFirstName());
+                txtLast.setText(c.getLastName());
+                txtPhone.setText(c.getPhone());
+                txtDoB.setText(c.getDateOfBirth());
+            }
 
             /*first = bundle.getString("first");
             last = bundle.getString("last");
             phone = bundle.getString("phone");
             dob = bundle.getString("dob");*/
         }
+        //back button listener ------- need to make it to go back to same view(showing same position on the list as before)
+        findViewById(R.id.btn_Detail_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DetailActivity.this, ListPageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/holmesglen/assesment2/Activities/MainActivity.java
+++ app/src/main/java/com/holmesglen/assesment2/Activities/MainActivity.java
@@ -1,11 +1,14 @@
 package com.holmesglen.assesment2.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.holmesglen.assesment2.Database.PhonebookDb;
@@ -15,124 +18,69 @@ import com.holmesglen.assesment2.R;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
-    private MyHash hash;
-    // for the recycler view instance in layout
-    private RecyclerView recyclerViewMainList;
+    private GestureDetectorCompat detector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_page);
 
+        detector = new GestureDetectorCompat(this, this);
+
+
+
+
         findViewById(R.id.btnDark).setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ListPageActivity.class);
-                startActivity(intent);}});}}
-       //on click go to the list page
+                startActivity(intent);
+            }
+        });
+    }
 
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
 
-
-    /*public void goDetail(View view)
-    {
-
-        setContentView(R.layout.detail_page);
-    }*/
-    /*private void doHash()
-    {
-        setContentView(R.layout.list_page);
-        //getting data from database and hash it
-        ArrayList<Contact> allContacts = PhonebookDb.getInstance().getAll();
-        hash = new MyHash();
-        hash.buildHashTable(allContacts);
-
-        //setting data for recyclerView
-        recyclerViewMainList = findViewById(R.id.recview1);
-
-        //step 1 create adapter
-        MainListRecyclerViewAdapter adapter = new MainListRecyclerViewAdapter(hash.toList(false));
-
-        //step 2 set adapter
-        recyclerViewMainList.setAdapter(adapter);
-
-        //step 3 set layout manager
-        recyclerViewMainList.setLayoutManager(new LinearLayoutManager(this));
-
-        //setting click function for all buttons
-        setAllNavBtnClickListener();
+    @Override
+    public void onShowPress(MotionEvent e) {
 
     }
-    private void navBtnClick(int key)
-    {
-        if(key<0 || key>26)
-        {
-            return;
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+    private static final int SWIPE_MIN_DISTANCE = 100;
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        if(e1.getX() - e2.getX()>SWIPE_MIN_DISTANCE) {
+            Intent intent = new Intent(MainActivity.this, ListPageActivity.class);
+            startActivity(intent);
+            return true;
         }
-        int offset = hash.calcOffsetByKey(key);
-        //scroll the view
-        ((LinearLayoutManager)recyclerViewMainList.getLayoutManager()).scrollToPositionWithOffset(offset, 0);
+        return false;
     }
-    private void setAllNavBtnClickListener()
-    {
-        findViewById(R.id.btn_hash).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(0);});
-        findViewById(R.id.btn_A).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(1);});
-        findViewById(R.id.btn_B).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(2);});
-        findViewById(R.id.btn_C).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(3);});
-        findViewById(R.id.btn_D).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(4);});
-        findViewById(R.id.btn_E).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(5);});
-        findViewById(R.id.btn_F).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(6);});
-        findViewById(R.id.btn_G).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(7);});
-        findViewById(R.id.btn_H).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(8);});
-        findViewById(R.id.btn_I).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(9);});
-        findViewById(R.id.btn_J).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(10);});
-        findViewById(R.id.btn_K).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(11);});
-        findViewById(R.id.btn_L).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(12);});
-        findViewById(R.id.btn_M).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(13);});
-        findViewById(R.id.btn_N).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(14);});
-        findViewById(R.id.btn_O).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(15);});
-        findViewById(R.id.btn_P).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(16);});
-        findViewById(R.id.btn_Q).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(17);});
-        findViewById(R.id.btn_R).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(18);});
-        findViewById(R.id.btn_S).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(19);});
-        findViewById(R.id.btn_T).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(20);});
-        findViewById(R.id.btn_U).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(21);});
-        findViewById(R.id.btn_V).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(22);});
-        findViewById(R.id.btn_W).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(23);});
-        findViewById(R.id.btn_X).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(24);});
-        findViewById(R.id.btn_Y).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(25);});
-        findViewById(R.id.btn_Z).setOnClickListener((v) -> {
-            MainActivity.this.navBtnClick(26);});
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        detector.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
-    public void backBtnClick(View view)
-    {
-        doHash();
-    }*/
+}
+
 
